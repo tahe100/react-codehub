3c453dce348097d499e298b46315845a6488cfcb
tehe100 <tahe100@uni-duesseldorf.de>
2023-12-27 00:23:00 +0100

    Injecting Token into Axios Request Headers

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,4 +1,11 @@
+//测试token是否成功注入
+import { useEffect } from "react"
+import { request } from "../../utils"
+
 const Layout = () => {
+    useEffect(()=>{
+        request.get('/user/profile')
+    },[])
     return <div> this is Layout</div>
 }
 
